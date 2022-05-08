75cec097e911b2634923c33a95131955206e13a2
Pavel <roigoci@gmail.com>
2022-05-08 12:53:01 +0200

    Fix random change of target city during day. Improve layout.

--- src/App.js
+++ src/App.js
@@ -17,35 +17,40 @@ function App() {
   const [guesses, setGuesses] = useState([]);
 
   useEffect(() => {
+    const random = (seed) => {
+      var x = Math.sin(seed) * 10000;
+      return x - Math.floor(x);
+    }
+
+    const getSeedFromDate = () => {
+      const dayNumber = Math.floor(Date.now() / 1000 / 24 / 60 / 60);
+      console.log("Seed from day is " + dayNumber);
+      return dayNumber;
+    }
+
+    const getRandCity = () => {
+      const city = cities[Math.floor(cities.length * random(getSeedFromDate()))];
+      console.log("Target city is " + city.name);
+      return city;
+    }
+
     setTargetCity(getRandCity());
-  })
-
-  const random = (seed) => {
-    var x = Math.sin(seed) * 10000;
-    return x - Math.floor(x);
-  }
-
-  const getSeedFromDate = () => {
-    return Date.now() / 1000 / 24 / 60 / 60;
-  }
-
-  const getRandCity = () => {
-    return cities[Math.floor(cities.length * random(getSeedFromDate()))];
-  }
+  }, []);
 
   const countDirection = (city1, city2) => {
+    const ratio = 3;
     const latitudeDiff = city2.latitude - city1.latitude; // NS
     const longtitudeDiff = city2.longtitude - city1.longtitude; // EW
 
     const latLongRatio = Math.abs(latitudeDiff)/Math.abs(longtitudeDiff);
-    if (latLongRatio > 2) {
+    if (latLongRatio > ratio) {
       // simple direction - NS
       if (latitudeDiff > 0) {
         return 'N';
       } else {
         return 'S';
       }
-    } else if (latLongRatio < 0.5) {
+    } else if (latLongRatio < 1/ratio) {
       // simlpe direction - EW
       if (longtitudeDiff > 0) {
         return 'E';
@@ -88,6 +93,10 @@ function App() {
       setCityPart('');
       setFilteredCities([]);
       setGuesses([...guesses, guessedCity]);
+      if (guessedCity.name === targetCity.name) {
+        // end of game
+        console.log("EOG");
+      }
     }
   }
 

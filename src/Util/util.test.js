6d5e21f8466f96c0262ecbf5510e609fe8c41f4a
Pavel Kubik <pavel.kubik@citrix.com>
2022-05-12 09:48:57 +0200

    Add tests for choice random city from date.

--- src/Util/util.test.js
+++ src/Util/util.test.js
@@ -1,5 +1,16 @@
-import { countDirection } from './util';
+import { countDirection, getSeedFromDate } from './util';
 
 test('test direction', () => {
-  expect(countDirection({latitude: -1, longtitude: 0}, {latitude: 1, longtitude: .3})).toBe('N');
+  expect(countDirection({latitude: -1, longtitude: 0}, {latitude: 1, longtitude: .3})).toBe('NW');
 })
+
+test('test date seed generator', () => {
+  jest.useFakeTimers('modern');
+  jest.setSystemTime(new Date("1 Jan 1970 00:00:00 GMT"));
+  expect(getSeedFromDate()).toBe(0);
+  jest.setSystemTime(new Date("1 Jan 1970 10:00:00 GMT"));
+  expect(getSeedFromDate()).toBe(0);
+  jest.setSystemTime(new Date("1 Jan 1970 23:00:00 GMT"));
+  expect(getSeedFromDate()).toBe(0);
+  jest.useRealTimers();
+});

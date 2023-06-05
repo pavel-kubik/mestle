0becfe29650f60a8fe053f039d06ce7b9edc668d
Pavel <roigoci@gmail.com>
2023-06-05 17:55:05 +0200

    Move auth functions to library.

--- netlify/functions/sign_in/sign_in.ts
+++ netlify/functions/sign_in/sign_in.ts
@@ -1,65 +1,6 @@
 import { Handler } from '@netlify/functions';
-import { connectToDatabase } from '../../util/dbUtil';
-
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+import { signIn } from 'jwt-auth-mongodb/dist/be/auth';
 
 export const handler: Handler = async (event, context) => {
-  if (event.httpMethod !== 'POST') {
-    return { statusCode: 404 };
-  }
-
-  if (!event.body) {
-    return { statusCode: 400 };
-  }
-
-  if (!process.env.TOKEN_KEY) {
-    console.log('MISCONFIGURATION - Missing TOKEN KEY');
-    return { statusCode: 500, body: 'Missing TOKEN KEY' };
-  }
-
-  const { email, password } = JSON.parse(event.body);
-
-  context.callbackWaitsForEmptyEventLoop = false;
-
-  try {
-    const database = await connectToDatabase(process.env.MONGODB_URI);
-    const collection = database.collection('user');
-
-    const userData = await collection.findOne({ email: email });
-
-    if (userData && userData.password) {
-      if (await bcrypt.compare(password, userData.password)) {
-        const token = jwt.sign(
-          { user_id: userData._id, email }, //
-          process.env.TOKEN_KEY, //
-          {
-            expiresIn: '7d'
-          }
-        );
-
-        return {
-          statusCode: 200,
-          headers: {
-            'x-access-token': token
-            // TODO store jwt in http only session
-          },
-          body: JSON.stringify({
-            id: userData._id,
-            username: userData.username,
-            token: token
-          })
-        };
-      }
-    }
-    return {
-      statusCode: 401,
-      body: JSON.stringify({
-        message: 'Username or password is incorrect'
-      })
-    };
-  } catch (error) {
-    console.log(error);
-    return { statusCode: 500, body: error.toString() };
-  }
+  return await signIn(event, context);
 };
--- netlify/functions/sign_in_salt/sign_in_salt.ts
+++ netlify/functions/sign_in_salt/sign_in_salt.ts
@@ -1,44 +1,6 @@
 import { Handler } from '@netlify/functions';
-import { connectToDatabase } from '../../util/dbUtil';
-
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+import { signInSalt } from 'jwt-auth-mongodb/dist/be/auth';
 
 export const handler: Handler = async (event, context) => {
-  if (event.httpMethod !== 'POST') {
-    return { statusCode: 404 };
-  }
-
-  if (!event.body) {
-    return { statusCode: 400 };
-  }
-
-  const { email } = JSON.parse(event.body);
-
-  context.callbackWaitsForEmptyEventLoop = false;
-
-  try {
-    const database = await connectToDatabase(process.env.MONGODB_URI);
-    const collection = database.collection('user');
-
-    const userData = await collection.findOne({ email: email });
-
-    if (userData) {
-      return {
-        statusCode: 200,
-        body: JSON.stringify({
-          salt: userData.salt
-        })
-      };
-    }
-    return {
-      statusCode: 401,
-      body: JSON.stringify({
-        message: 'Username or password is incorrect'
-      })
-    };
-  } catch (error) {
-    console.log(error);
-    return { statusCode: 500, body: error.toString() };
-  }
+  return await signInSalt(event, context);
 };
